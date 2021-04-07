022aad6df1df91baf87efa03d499a871091d5faa
idougwarner <idougwarner@gmail.com>
2021-04-07 06:21:31 -0400

    update

--- handler.js
+++ handler.js
@@ -2,5 +2,12 @@
 
 module.exports.run = async (event, context) => {
   const time = new Date();
-  console.log(`Your cron function "${context.functionName}" ran at ${time} with env ${process.env.TEST_ENV}`);
+  const db = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  };
+  console.log(`Your cron function "${context.functionName}" ran at ${time} with db ${JSON.stringify(db, null, 2)}`);
 };

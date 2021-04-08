551631d5de141cdae2fd478b267a59717dcee49d
Mark Mbithi <markmbila@gmail.com>
2021-04-08 23:49:30 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: 'postgre',
+    dialect: 'postgres',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT
   }

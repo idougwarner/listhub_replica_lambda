9383558e362ab5fe9c3025df6240c3edabb84411
Mark Mbithi <markmbila@gmail.com>
2021-04-09 20:39:36 +0300

    Update url.js

--- config/url.js
+++ config/url.js
@@ -1,5 +1,5 @@
 module.exports = {
-    metaURL: "https://api.listhub.com/replication/metadata",
+    metaURL: "http://api.listhub.com/public_sandbox/replication/metadata",
     replicationURL: "http://api.listhub.com/public_sandbox/replication/query?select=ListingKey",
     token: "eyJraWQiOiI1c0RNZ1dhbDlSUXFyLTJDNVRmNVFES3I3MGZSMXM3a202X1FzUUxWb2w4IiwiYWxnIjoiUFMyNTYifQ.eyJ1c2VyIjp7ImNsaWVudElkIjoicmVkZWZ5cmVuIiwiY2hhbm5lbElkIjo1MDh9LCJzdWIiOiJyZWRlZnlyZW4iLCJhdWQiOiJhcGkubGlzdGh1Yi5jb20iLCJpc3MiOiJ0cnVzcy5saXN0aHViLm1vdmVhd3MuY29tIiwiaWF0IjoxNjE3OTIyNzUxLCJleHAiOjE2MTgwMDkxNTF9.EqHuwKFp_ez91hF7H5Z5LqSWqZD3YB8ln3opDXypwBO9U4Vag8xUVqSedt2wo9p45HDEkCjMhN8kqN4cLnWawYU7iENd1BktEhJL826Ct--BcghtAbmUxpAozt8CY62z4lx0FagQgw8MuuQop59uuitQP8jBoc1JadDeKB9iOPYOcVmHC4VDoiTD1_C4YEjnfdv8S90rSn1ofxOupdEE-XvOJ3n9p4ij82fwWNhZ3dBv7T3NJkyqI37nFuZwLqubDeG7izNDNHZiTL3nTpo_4Ccw78M1b_9A-o2KjGja6o0wh_avfzy961OWMsaOTHYURDBupjgMnBD5abvxEP9X_w"
   };

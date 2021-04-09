872120c92210984421b3c4167244cc6c505bdb66
Mark Mbithi <markmbila@gmail.com>
2021-04-09 20:18:53 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -177,6 +177,9 @@ const fetchListingData = async (type) => {
 
     console.log("After create a file write stream");
 
+    var listdataAdded;
+    var listError;
+
     inputStream
       .on("response", (response) => {
         console.log("Status code " + response.statusCode);
@@ -207,27 +210,29 @@ const fetchListingData = async (type) => {
         // Create All Property Listings at once
 
         const { dataAdded, error } = await propertyBulkCreate(listings1);
-
-        if (dataAdded) {
-          // Data was successfully added therefore add step and downloadedSize and proceed to get next chunk in next loop
-          step = step + 1;
-
-          chunkDownloaded = step + 1; // We want to know how many chunks are downloaded
-          downloadedSize = endOfRange;
-          remainingDownloadSize = filetoDownloadSize - downloadedSize;
-
-          result.listAddError = null;
-          result.listdataAdded = true;
-          result.datadownloaded = downloadedSize;
-          result.remainingToDownload = remainingDownloadSize;
-        } else {
-          remainingDownloadSize = filetoDownloadSize - downloadedSize;
-          result.remainingToDownload = remainingDownloadSize;
-          result.listAddError = error;
-
-          break;
-        }
+        listdataAdded=dataAdded;
+        listError=error
       }); // End of Input Stream
+
+      if (listdataAdded) {
+        // Data was successfully added therefore add step and downloadedSize and proceed to get next chunk in next loop
+        step = step + 1;
+
+        chunkDownloaded = step + 1; // We want to know how many chunks are downloaded
+        downloadedSize = endOfRange;
+        remainingDownloadSize = filetoDownloadSize - downloadedSize;
+
+        result.listAddError = null;
+        result.listdataAdded = listdataAdded;
+        result.datadownloaded = downloadedSize;
+        result.remainingToDownload = remainingDownloadSize;
+      } else {
+        remainingDownloadSize = filetoDownloadSize - downloadedSize;
+        result.remainingToDownload = remainingDownloadSize;
+        result.listAddError = listError;
+
+        break;
+      }
   } // End While
 
   return result;

d5c78e39622eebd4eb7d5ee1837045320b14a700
Mark Mbithi <markmbila@gmail.com>
2021-04-14 20:59:04 +0300

    Update propertymeta.controller.js

--- controllers/propertymeta.controller.js
+++ controllers/propertymeta.controller.js
@@ -163,9 +163,11 @@ module.exports.ismetadataNew = async (lastModified) => {
 
       }
     }
+
     else {
       console.log("No New MetaData")
     }
+    
   } catch (err) {
     const result = {
       newUpdate: false,

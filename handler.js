d0847027809347d24d5f04bf113c47264f0aa1e4
Mark Mbithi <markmbila@gmail.com>
2021-04-16 18:48:31 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -592,7 +592,7 @@ const fetchData = async () => {
 
   catch(err) {
 
-    console.log(err.response.status)
+    console.log(err)
     
     if(err.response.status==401) {
       console.log("You are not authorized to access Listhub API Obtain acces from admin")

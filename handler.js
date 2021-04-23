e20ec7eaada2edc0a638f3db1ebfa33f1b1c76e2
Mark Mbithi <markmbila@gmail.com>
2021-04-23 19:09:49 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -634,7 +634,7 @@ module.exports.streamExecutor = async (event, context, callback) => {
     stream.pipe(JSONStream.parse()).pipe(
       es.mapSync((data) => {
         listingArray.push(data);
-        console.log("Data " + data)
+        console.log("Data Sequence " + data.sequence + " Property " + data.Property)
       })
     );
 

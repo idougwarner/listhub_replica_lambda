35371dac732b5d97169bea0285b90e1564094762
idougwarner <idougwarner@gmail.com>
2021-04-23 11:13:30 -0400

    update

--- handler.js
+++ handler.js
@@ -756,21 +756,3 @@ module.exports.testfetchListingsData = (event, context, callback) => {
       */
     });
 };
-
-module.exports.run = (event, context) => {
-  /*const time = new Date();
-
-  const db = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    name: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  };
-
-  console.log(
-    `Your cron function "${
-      context.functionName
-    }" ran at ${time} with db ${JSON.stringify(db, null, 2)}`
-  );*/
-};

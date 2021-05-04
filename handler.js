131961f8101de9c89f27d28d9007db427ef4b800
idougwarner <idougwarner@gmail.com>
2021-05-04 19:45:51 -0400

    update

--- handler.js
+++ handler.js
@@ -346,7 +346,7 @@ module.exports.streamExecutor = async (event, context, callback) => {
         for (let index = 0; index < listingsCount; index += 1) {
           const listing = listingArray[index];
           if (!listing || !listing.sequence || !listing.Property || listing.statusCode === 412) continue;
-          const query = `INSERT INTO ${tableName} (listing_id, address, city, state, property) VALUES ($1,$2)`;
+          const query = `INSERT INTO ${tableName} (listing_id, address, city, state, property) VALUES ($1,$2,$3,$4,$5)`;
           const variables =  [listing.ListingKey, listing.UnparsedAddress, listing.PostalCity, listing.StateOrProvince, listing.Property];
           await sendQuery(query, variables);
         }
